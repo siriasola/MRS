e0982f7f26c3305d6950f953e74462ad87e5db23
siriasola <solasiria@gmail.com>
2025-02-04 00:08:02 +0100

    creazione del main.py per implementazione futura

---
+++ main.py
@@ -0,0 +1,11 @@
+import os 
+import pandas as pd
+
+def main():
+
+
+
+
+
+    if __name__ == "__main__": #Ultime righe che servono per controllare se lo script viene eseguito direttamente
+        main()

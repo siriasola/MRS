02beab3ee2192487a112532dd2c6ec99ddc210c3
siriasola <solasiria@gmail.com>
2025-02-06 22:11:35 +0100

    Aggiunta commenti all'evaluation_tests

--- tests/evaluation_test.py
+++ tests/evaluation_test.py
@@ -5,8 +5,10 @@ from validation.evaluation import Evaluation
 
 class TestEvaluation(unittest.TestCase):
     def setUp(self):
-        """Inizializza i dati di test."""
-        np.random.seed(42)
+        """Inizializza i dati di test. Viene eseguito prima di ogni test per creare un dataset di prova.
+          Genera 100 campioni casuali con 5 feature e una variabile target binaria."""
+        
+        np.random.seed(42) #Fissa la sequenza casuale per ottenere risultati riproducibili.
         self.features = pd.DataFrame(np.random.rand(100, 5))
         self.target = pd.Series(np.random.choice([0, 1], size=100))
         self.k_folds = 5
@@ -15,7 +17,10 @@ class TestEvaluation(unittest.TestCase):
         self.evaluator = Evaluation(self.features, self.target, self.k_folds, self.metriche_scelte, self.k)
     
     def test_valutazione_k_fold(self):
-        """Testa la validazione K-Fold."""
+
+        """Testa la validazione K-Fold. 
+        Controlla che i risultati siano un dizionario e che contengano tutte le metriche richieste.
+        """
         risultati = self.evaluator.valutazione_k_fold()
         self.assertIsInstance(risultati, tuple)  # Ora gestiamo una tupla
         self.assertIsInstance(risultati[0], dict)  # Il primo elemento deve essere il dizionario delle metriche
@@ -23,13 +28,18 @@ class TestEvaluation(unittest.TestCase):
 
     
     def test_valutazione_leave_one_out(self):
-        """Testa la validazione Leave-One-Out."""
+
+        """Testa la validazione Leave-One-Out.
+        Controlla che restituisca una tupla e un dizionario con tutte le metriche richieste.
+        """
+
         risultati = self.evaluator.valutazione_leave_one_out()
         self.assertIsInstance(risultati, tuple)
         self.assertIsInstance(risultati[0], dict)
         self.assertTrue(all(m in risultati[0] for m in self.metriche_scelte))
 
     def test_valutazione_holdout(self):
+        
         """Testa la validazione Holdout."""
         risultati = self.evaluator.valutazione_holdout()
         self.assertIsInstance(risultati, tuple)

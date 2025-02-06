63c45944138531877905252492d97cf1db9f2f33
DicaRebby <rebeccad.c02@gmail.com>
2025-02-06 18:37:23 +0100

    avevamo importato due volte la stessa libreria

--- validation/visualizzazione.py
+++ validation/visualizzazione.py
@@ -2,9 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 def plot_confusion_matrix(y_true, y_pred):
     """
     Genera e visualizza una Confusion Matrix.
